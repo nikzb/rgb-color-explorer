fc5da913309699b6afe79f7d4039f1775b43a57e
Nik <NikBaltatzis@gmail.com>
2017-04-29 22:12:10 -0400

    First version of circle join animation

--- src/classes/ColorCircleGroup/ColorCircleGroup.js
+++ src/classes/ColorCircleGroup/ColorCircleGroup.js
@@ -4,55 +4,75 @@ import ColorCode from '../ColorCode/ColorCode';
 const RT_3_OVER_2 = Math.sqrt(3) / 2;
 const ONE_HALF = 0.5;
 // A measure of how much the circles will overlap, bigger is more overlap
-const CIRCLE_TIGHTNESS = 4;
+const MAX_CIRCLE_DIST_MULT = 0.25;
 
 class ColorCircleGroup {
   // The constructor takes a ColorCode and a canvas context.
   // It creates an array of three circles make up of the three color components
-  constructor({colorCode, context, x, y, radius}) {
+  constructor({colorCode, context, x, y, radius, percentFromCenter}) {
     this.circles = [];
-    // Add red circle in bottom left position
-    this.circles.push(new ColorCircle({
-      x: x - radius * RT_3_OVER_2 / CIRCLE_TIGHTNESS,
-      y: y - radius * ONE_HALF / CIRCLE_TIGHTNESS,
-      radius: radius,
-      colorCode: new ColorCode({
-        base: colorCode.getBase(),
-        bits: colorCode.getBits(),
-        red: colorCode.getComponent('R'),
-        green: 0,
-        blue: 0,
-      }),
-      context
-    }));
-    // Add green circle in top position
-    this.circles.push(new ColorCircle({
-      x: x,
-      y: y + radius / CIRCLE_TIGHTNESS,
-      radius: radius,
-      colorCode: new ColorCode({
-        base: colorCode.getBase(),
-        bits: colorCode.getBits(),
-        red: 0,
-        green: colorCode.getComponent('G'),
-        blue: 0,
-      }),
-      context
-    }));
-    // Add blue circle in bottom right position
-    this.circles.push(new ColorCircle({
-      x: x + radius * RT_3_OVER_2 / CIRCLE_TIGHTNESS,
-      y: y - radius * ONE_HALF / CIRCLE_TIGHTNESS,
-      radius: radius,
-      colorCode: new ColorCode({
-        base: colorCode.getBase(),
-        bits: colorCode.getBits(),
-        red: 0,
-        green: 0,
-        blue: colorCode.getComponent('B'),
-      }),
-      context
-    }));
+    this.percentFromCenter = percentFromCenter;
+
+    console.log(this.percentFromCenter);
+    if (Math.abs(this.percentFromCenter) < 0.01) {
+      this.circles.push(new ColorCircle({
+        x: 0,
+        y: 0,
+        radius: radius,
+        colorCode: new ColorCode({
+          base: colorCode.getBase(),
+          bits: colorCode.getBits(),
+          red: colorCode.getComponent('R'),
+          green: colorCode.getComponent('G'),
+          blue: colorCode.getComponent('B')
+        }),
+        context
+      }));
+    }
+    else {
+      // Add red circle in bottom left position
+      this.circles.push(new ColorCircle({
+        x: x - radius * MAX_CIRCLE_DIST_MULT * this.percentFromCenter * RT_3_OVER_2,
+        y: y - radius * MAX_CIRCLE_DIST_MULT * this.percentFromCenter * ONE_HALF,
+        radius: radius,
+        colorCode: new ColorCode({
+          base: colorCode.getBase(),
+          bits: colorCode.getBits(),
+          red: colorCode.getComponent('R'),
+          green: 0,
+          blue: 0,
+        }),
+        context
+      }));
+      // Add green circle in top position
+      this.circles.push(new ColorCircle({
+        x: x,
+        y: y + radius * MAX_CIRCLE_DIST_MULT * this.percentFromCenter,
+        radius: radius,
+        colorCode: new ColorCode({
+          base: colorCode.getBase(),
+          bits: colorCode.getBits(),
+          red: 0,
+          green: colorCode.getComponent('G'),
+          blue: 0,
+        }),
+        context
+      }));
+      // Add blue circle in bottom right position
+      this.circles.push(new ColorCircle({
+        x: x + radius * MAX_CIRCLE_DIST_MULT * this.percentFromCenter * RT_3_OVER_2,
+        y: y - radius * MAX_CIRCLE_DIST_MULT * this.percentFromCenter * ONE_HALF,
+        radius: radius,
+        colorCode: new ColorCode({
+          base: colorCode.getBase(),
+          bits: colorCode.getBits(),
+          red: 0,
+          green: 0,
+          blue: colorCode.getComponent('B'),
+        }),
+        context
+      }));
+    }
   }
 
   draw() {
--- src/components/ColorViz/ColorViz.js
+++ src/components/ColorViz/ColorViz.js
@@ -7,22 +7,31 @@ import './ColorViz.css';
 
 const CANVAS_WIDTH = 400;
 const CANVAS_HEIGHT = 400;
+const PERCENT_CHANGE = 0.03;
 
 class ColorViz extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      percentFromCenter: 1.00
+      // wasShowingComponents: true,
+      // transition: 'join', // 'join' or 'separate'
+      // transitionProgress: 0
+    }
+    this.updateViz = this.updateViz.bind(this);
+  }
 
-  updateCanvas() {
+  drawCircleGroup() {
     const vizCanvas = ReactDOM.findDOMNode(this.refs.vizCanvas);
     const context = vizCanvas.getContext('2d');
-    context.globalCompositeOperation = "lighter";
 
+    console.log("updating viz " + this.state.percentFromCenter);
     const circleGroup = new ColorCircleGroup({
       colorCode: this.props.colorCode,
       x: 0,
       y: 0,
       radius: 150,
+      percentFromCenter: this.state.percentFromCenter,
       context
     });
 
@@ -31,8 +40,64 @@ class ColorViz extends Component {
     // Translate canvas coordinate system before drawing
     context.save();
     context.translate(vizCanvas.width / 2, vizCanvas.height / 2);
+    context.rotate(this.state.percentFromCenter * 2 * Math.PI);
     circleGroup.draw();
     context.restore();
+  }
+
+  updateViz() {
+    this.drawCircleGroup();
+
+    this.setState({
+      percentFromCenter: this.state.percentFromCenter - PERCENT_CHANGE
+    });
+
+    if (this.state.percentFromCenter > 0) {
+      requestAnimationFrame(this.updateViz);
+    } else {
+      this.setState({
+        percentFromCenter: 0
+      });
+    }
+  }
+
+  updateCanvas() {
+
+    console.log('update canvas');
+    const vizCanvas = ReactDOM.findDOMNode(this.refs.vizCanvas);
+    const context = vizCanvas.getContext('2d');
+    context.globalCompositeOperation = "lighter";
+
+    // Was showing and hide was just clicked
+    console.log('percent from center ' + this.state.percentFromCenter);
+    console.log('show' + this.props.showColorComponents);
+    //debugger;
+    if (this.state.percentFromCenter === 1.00 && !this.props.showColorComponents) {
+      console.log('updating state first time');
+      this.setState({
+        percentFromCenter: this.state.percentFromCenter - PERCENT_CHANGE
+      });
+      requestAnimationFrame(this.updateViz);
+    } else {
+      this.drawCircleGroup();
+    }
+
+    // // Was showing and hide was just clicked
+    // if (this.state.wasShowingComponents && !this.props.showColorComponents) {
+    //   // Need to initiate join transition
+    //   this.setState({
+    //     wasShowingComponents: false
+    //   });
+    //   joinTransition();
+    // }
+    // else if (!this.state.wasShowingComponents && this.props.showColorComponents) {
+    //   // Need to initiate separate transition
+    //   this.setState({
+    //     wasShowingComponents: true
+    //   });
+    // }
+
+
   }
 
   componentDidMount() {
