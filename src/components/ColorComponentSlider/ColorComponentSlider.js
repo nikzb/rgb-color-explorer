528aa08bcfca8370e1a4809737d6697fb0768629
Nik <NikBaltatzis@gmail.com>
2017-05-03 20:51:39 -0400

    Color buttons can toggle colors on and off

--- src/components/ColorComponentSlider/ColorComponentSlider.js
+++ src/components/ColorComponentSlider/ColorComponentSlider.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
 import UpDownControl from '../UpDownControl/UpDownControl';
+import ColorToggleButton from '../ColorToggleButton/ColorToggleButton';
 
 import './ColorComponentSlider.css';
 
 class ColorComponentSlider extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      toggledOff: false,
+      savedValue: {
+        intensity: 0,
+        max: this.props.max
+      }
+    };
     this.handleChange = this.handleChange.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle() {
+    const comp = this.props.colorComp.charAt(0);
+
+    if (this.state.toggledOff) {
+      this.setState({
+        toggledOff: false
+      });
+
+      // Calculate the correct intensity to use. This needs to be done because the number of bits used may have changed.
+      const newIntensity = Math.round(this.state.savedValue.intensity / this.state.savedValue.max * this.props.max);
+
+      this.props.onColorChange({
+        comp,
+        newValue: newIntensity
+      });
+    } else {
+      this.setState({
+        toggledOff: true,
+        savedValue: {
+          intensity: this.props.intensity,
+          max: this.props.max
+        }
+      });
+      this.props.onColorChange({
+        comp,
+        newValue: 0
+      })
+    }
   }
 
   handleChange(e) {
-    console.log(e.target.value);
+    this.setState({
+      toggledOff: false,
+    });
     this.props.onColorChange({
       comp: this.props.colorComp.charAt(0),
       newValue: parseInt(e.target.value, 10)
@@ -22,20 +63,23 @@ class ColorComponentSlider extends Component {
   // @param oldValue - the current value of the component that will be adjusted when a button is clicked
   updateColor(comp, oldValue) {
     return (changeByAmount) => {
+      this.setState({
+        toggledOff: false
+      });
       this.props.onColorChange({
         comp,
         newValue: oldValue + changeByAmount
-      })
+      });
     };
   }
 
   render() {
-    const labelClassNames = `ColorComponentSlider__label ColorComponentSlider__label--${this.props.colorComp.toLowerCase()}`;
     const containerClassNames = `ColorComponentSlider__container ColorComponentSlider__container--${this.props.colorComp.toLowerCase()}`;
 
     return (
       <div className={containerClassNames}>
-        <label className={labelClassNames}>{this.props.colorComp}</label>
+        {/* <label className={labelClassNames}>{this.props.colorComp}</label> */}
+        <ColorToggleButton onClick={this.handleToggle} colorComp={this.props.colorComp} toggledOff={this.state.toggledOff}/>
         <input className='ColorComponentSlider__slider' type='range' min={0} max={this.props.max} value={this.props.intensity} step={1} onChange={this.handleChange}/>
         <UpDownControl onClick={this.updateColor(this.props.colorComp.charAt(0), this.props.intensity)}/>
       </div>
--- src/components/ColorToggleButton/ColorToggleButton.js
+++ src/components/ColorToggleButton/ColorToggleButton.js
@@ -5,7 +5,16 @@ import './ColorToggleButton.css';
 class ColorToggleButton extends Component {
 
   render() {
+    let classNames = `button ColorToggleButton ColorToggleButton--${this.props.colorComp.toLowerCase()}`;
+    console.log(classNames);
 
+    if (this.props.toggledOff) {
+      classNames += ' ColorToggleButton--toggled-off';
+    }
+
+    return (
+      <button className={classNames} onClick={this.props.onClick}>{this.props.colorComp}</button>
+    );
   }
 }
 
