0f07760914e46c0b4f7537180ed43a443e1dbcc4
Nik <NikBaltatzis@gmail.com>
2017-05-08 22:16:03 -0400

    removed Helmet import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import MainSection from '../MainSection/MainSection';
-import {Helmet} from 'react-helmet';
+// import {Helmet} from 'react-helmet';
 
 require('foundation-sites/dist/css/foundation.min.css');
 
