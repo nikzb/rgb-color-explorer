3665a4ca6c12c7ca039865244c8bedc28220082a
Nik <NikBaltatzis@gmail.com>
2017-05-10 10:09:40 -0400

    try server without https

--- src/server.js
+++ src/server.js
@@ -6,13 +6,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect(`http://${req.hostname}${req.url}`);
-  } else {
-    next();
-  }
-});
+// app.use(function (req, res, next) {
+//   if (req.headers['x-forwarded-proto'] === 'https') {
+//     res.redirect(`http://${req.hostname}${req.url}`);
+//   } else {
+//     next();
+//   }
+// });
 
 console.log(__dirname);
 
