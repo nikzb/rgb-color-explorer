651cda72de321ab590114cfc3198510304cbe43d
Nik <NikBaltatzis@gmail.com>
2017-05-10 14:15:12 -0400

    fixed paths in App files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import MainSection from './components/MainSection/MainSection';
+import Header from '../Header/Header';
+import MainSection from '../MainSection/MainSection';
 // import {Helmet} from 'react-helmet';
 
 require('foundation-sites/dist/css/foundation.min.css');
