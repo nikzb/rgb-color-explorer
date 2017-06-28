c60b9b56afa348824f2a14a6239c4ef4f143a46d
Nik <NikBaltatzis@gmail.com>
2017-06-27 20:04:13 -0400

    fixed double click to save extra flip and apply after current one is finished

--- src/components/BinaryCountingMain/BinaryCountingMain.js
+++ src/components/BinaryCountingMain/BinaryCountingMain.js
@@ -10,78 +10,67 @@ class BinaryCountingMain extends Component {
     super(props);
     this.state = {
       bitList: List([
-        Map({angle: 0, onClick: this.getClickHandler(0)}),
-        Map({angle: 0, onClick: this.getClickHandler(1)}),
-        Map({angle: 0, onClick: this.getClickHandler(2)})
+        Map({angle: 0, extraRotation: false, onClick: this.getClickHandler(0)}),
+        Map({angle: 0, extraRotation: false, onClick: this.getClickHandler(1)}),
+        Map({angle: 0, extraRotation: false, onClick: this.getClickHandler(2)})
       ])
     };
-      // bitInfoArray: [
-      //   {angle: 0, interval: null},
-      //   {angle: 0, interval: null},
-      //   {angle: 0, interval: null}
-      // ]
   }
 
   getClickHandler(index) {
     return () => {
-      this.initiateFlip(index, true);
+      this.initiateFlip(index);
     }
   }
 
   // @param index: the index of the bit panel to be rotated
   // @param firstIter: boolean value signifying if this is the initial rotation of this bit panel within this flip
   // @param shouldPropagate: boolean value signifying if a panel being flipped to 0 should cause the next panel to its left to flip
-  rotate({index, firstIter, shouldPropagate, fromClick}) {
-    const bitList = this.state.bitList;
-    const angle = bitList.get(index).get('angle');
+  rotate({index, firstIter, shouldPropagate}) {
+    const angle = this.state.bitList.get(index).get('angle');
 
     const rotateOneDegree = (index) => {
       this.setState({
-        bitList: bitList.update(index, mapThingy => mapThingy.set('angle', (angle + 1) % 360))
+        bitList: this.state.bitList.update(index, mapThingy => mapThingy.set('angle', (angle + 1) % 360))
       });
     };
 
-    // if (firstIter || (angle !== 0 && angle !== 180)) {
-    //   rotateOneDegree(index);
-    //   setTimeout(() => { this.rotate(index, false) }, 2);
-    // } else if (angle === 0) {
-    //   this.initiateFlip(index + 1);
-    // }
-
-    // if (firstIter || (angle !== 0 && angle !== 180)) {
-    //   rotateOneDegree(index);
-    //   let time = 10;
-    //   if (!fromClick) {
-    //     time = 1;
-    //   }
-    //   setTimeout(() => { this.rotate({index, firstIter: false, shouldPropagate: true, fromClick}) }, time);
-    // }
-    //
-    // if (shouldPropagate && angle === 181) {
-    //   this.initiateFlip(index + 1, false);
-    // }
-
     if (firstIter || (angle !== 0 && angle !== 180)) {
       rotateOneDegree(index);
-      let time = 11.8;
-      if (!fromClick) {
-        time = 1;
+      setTimeout(() => { this.rotate({index, firstIter: false, shouldPropagate: true}) }, 2);
+    } else {
+      console.log(index + ' ' + this.state.bitList.get(index).get('extraRotation'));
+      if (this.state.bitList.get(index).get('extraRotation')) {
+        this.setState({
+          bitList: this.state.bitList.update(index, mapThingy => mapThingy.set('extraRotation', false))
+        });
+        rotateOneDegree(index);
+        setTimeout(() => { this.rotate({index, firstIter: false, shouldPropagate: true}) }, 2);
       }
-      setTimeout(() => { this.rotate({index, firstIter: false, shouldPropagate: true, fromClick}) }, 2);
     }
 
     if (shouldPropagate && angle === 181) {
       this.initiateFlip(index + 1, false);
     }
+  }
 
-
+  isRotating(index) {
+    const angle = this.state.bitList.get(index).get('angle');
+    return angle !== 0 && angle !== 180;
   }
 
   // @param index: the index of the bit panel to be flipped
-  initiateFlip(index, fromClick) {
-    // debugger;
-    if (index < this.state.bitList.size) {
-      this.rotate({index, firstIter: true, shouldPropagate: true, fromClick});
+  initiateFlip(index) {
+    const bitList = this.state.bitList;
+    if (index < bitList.size) {
+      if (this.isRotating(index) || bitList.get(index).get('extraRotation')) {
+        this.setState({
+          //bitList: bitList.get(index).set('extraRotation', true)
+          bitList: bitList.update(index, mapThingy => mapThingy.set('extraRotation', true))
+        });
+      } else {
+        this.rotate({index, firstIter: true, shouldPropagate: true});
+      }
     }
   }
 
